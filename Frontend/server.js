2bab232e9bfffbb99e630c642ca6dd98b7b24141
Ian Leitch <port001@gmail.com>
2020-06-05 20:25:58 +0200

    Improve demonstration.

--- Frontend/server.js
+++ Frontend/server.js
@@ -3,7 +3,7 @@ const port = 8080
 
 const server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" })
-  response.end("Hello World\n")
+  response.end(`Hello World: ${process.env.MY_NODE_ENV}`)
 })
 
 server.listen(port)
